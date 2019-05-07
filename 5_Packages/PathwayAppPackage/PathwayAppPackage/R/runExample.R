d5309b3171e4a8ee2d65db014d69ead53efe43b8
VasiRo <druchkivstats@gmail.com>
2019-05-07 21:48:10 +0200

    Packaging App

--- 5_Packages/PathwayAppPackage/PathwayAppPackage/R/runExample.R
+++
@@ -1,8 +0,0 @@
-runExample <- function() {
-  appDir <- system.file("shiny-examples", "PathwayApp", package = "PathwayAppPackage")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `PathwayAppPackage`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
