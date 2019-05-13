b1e0d88d7a5ee16f16ba24fdf27342b664be1f5e
VasiRo <druchkivstats@gmail.com>
2019-05-13 23:46:40 +0200

    Working on memoria

--- 5_Packages/PathwayApp/PathwayApp/R/runPathwayApp.R
+++ 5_Packages/PathwayApp/PathwayApp/R/runPathwayApp.R
@@ -1,4 +1,16 @@
 runPathwayApp <- function() {
+  if(!require(clusterProfiler))BiocManager::install("clusterProfiler")
+  if(!require(pathview))BiocManager::install("pathview")
+  if(!require(ReactomePA))BiocManager::install("ReactomePA")
+  if(!require(dplyr))install.packages("dplyr")
+  if(!require(shiny))install.packages("shiny")
+  if(!require(shinydashboard))install.packages("shinydashboard")
+  if(!require(ggplot2))install.packages("ggplot2")
+  if(!require(ggplot2))install.packages("shinycssloaders")
+  if(!require(knitr))install.packages("knitr")
+  if(!require(kableExtra))install.packages("kableExtra")
+  if(!require(formattable))install.packages("formattable")
+  if(!require(pathviewPatched))devtools::install_github("vdruchkiv/TFM/5_Packages/pathviewPatched")
   appDir <- system.file("shiny-examples", "PathwayApp", package = "PathwayApp")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `PathwayApp`.", call. = FALSE)
